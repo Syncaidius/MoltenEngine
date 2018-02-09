e119751f72775a122ea014bc8d2f1e458829a26f
Syncaidius <syncaidius@googlemail.com>
2018-02-09 13:18:57 +0000

    Summaries for IIndexedMesh.

--- Molten.Render/Mesh/IIndexedMesh.cs
+++ Molten.Render/Mesh/IIndexedMesh.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 
 namespace Molten.Graphics
 {
+    /// <summary>A mesh which uses an index buffer to reduce the amount of vertex data, by referring to vertices by their ID in the index buffer, 
+    /// essentially allowing the same vertex to be referenced multiple times.</summary>
+    /// <typeparam name="T">The type of vertex data the mesh should hold.</typeparam>
     public interface IIndexedMesh<T> : IMesh<T> where T : struct, IVertexType
     {
+        /// <summary>Copies the provided index data to the current mesh.</summary>
+        /// <typeparam name="I">The type of data to set.</typeparam>
+        /// <param name="data">The data to be copied.</param>
         void SetIndices<I>(I[] data) where I : struct;
 
+        /// <summary>Copies the provided index data to the current mesh.</summary>
+        /// <typeparam name="I">The type of data to set.</typeparam>
+        /// <param name="count">The number of elements in the dat array to copy.</param>
+        /// <param name="data">The data to be copied.</param>
         void SetIndices<I>(I[] data, int count) where I : struct;
 
-        void SetIndices<I>(I[] data, int offsetIndex, int count) where I : struct;
+        /// <summary>Copies the provided index data to the current mesh.</summary>
+        /// <typeparam name="I">The type of data to set.</typeparam>
+        /// <param name="count">The number of elements in the dat array to copy.</param>
+        /// <param name="data">The data to be copied.</param>
+        /// <param name="startIndex">The element within the data array to start copying from.</param>
+        void SetIndices<I>(I[] data, int startIndex, int count) where I : struct;
 
         /// <summary>Gets the maximum number of indices the mesh can contain.</summary>
         int MaxIndices { get; }
