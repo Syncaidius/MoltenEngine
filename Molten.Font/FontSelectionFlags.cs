c1c1c068b29ae7526ab5855a6f3193eba09910de
Syncaidius <syncaidius@googlemail.com>
2018-02-20 21:02:45 +0000

    Added 'OS/2' font table support.

---
+++ Molten.Font/FontSelectionFlags.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten.Font
+{
+    [Flags]
+    public enum FontSelectionFlags : ushort
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Default = 0,
+
+        /// <summary>
+        /// Font contains italic or oblique characters, otherwise they are upright.
+        /// </summary>
+        Italic = 1,
+
+        /// <summary>
+        /// Characters are underscored.
+        /// </summary>
+        Underscore = 2,
+
+        /// <summary>
+        /// Characters have their foreground and background reversed.
+        /// </summary>
+        Negative = 4,
+
+        /// <summary>
+        /// Outline (hollow) characters, otherwise they are solid.
+        /// </summary>
+        Outlined = 8,
+
+        /// <summary>
+        /// Characters are overstruck.
+        /// </summary>
+        Strikeout = 16,
+
+        /// <summary>
+        /// Characters are emboldened.
+        /// </summary>
+        Bold = 32,
+
+        /// <summary>
+        /// Characters are in the standard weight/style for the font.
+        /// </summary>
+        Regular = 64,
+
+        /// <summary>
+        /// If set, it is strongly recommended to use OS/2.sTypoAscender - OS/2.sTypoDescender+ OS/2.sTypoLineGap as a value for default line spacing for this font.
+        /// </summary>
+        UseTypoMetrics = 128,
+
+        /// <summary>
+        /// The font has ‘name’ table strings consistent with a weight/width/slope family without requiring use of ‘name’ IDs 21 and 22. (Please see more detailed description below.)
+        /// </summary>
+        Wws = 256,
+
+        /// <summary>
+        /// Font contains oblique characters.
+        /// </summary>
+        Oblique = 512,
+    }
+}
---
+++ Molten.Font/FontWidthClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten.Font
+{
+    /// <summary>
+    /// Indicates a relative change from the normal aspect ratio (width to height ratio) as specified by a font designer for the glyphs in a font.
+    /// </summary>
+    public enum FontWidthClass : ushort
+    {
+        None = 0,
+
+        UltraCondensed = 1,
+
+        ExtraCondensed = 2,
+
+        Condensed = 3,
+
+        SemiCondensed = 4,
+
+        Medium = 5,
+
+        SemiExpanded = 6,
+
+        Expanded = 7,
+
+        ExtraExpanded = 8,
+
+        UltraExpanded = 9,
+    }
+}
