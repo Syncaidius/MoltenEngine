6f12fb505f4e5267f96fc9ef33617d3d6f7c232f
Syncaidius <syncaidius@googlemail.com>
2018-03-24 23:09:39 +0000

    Starting over with CFF.

--- Molten.Font/SubTables/CFF/CFFIndexTable.cs
+++ Molten.Font/SubTables/CFF/CFFIndexTable.cs
@@ -44,56 +44,35 @@ namespace Molten.Font
             uint objectDataOffset = headerSize + offsetArraySize;
             uint objectDataStreamOffset = objectDataOffset + (uint)Header.StreamOffset;
 
+            Func<EnhancedBinaryReader, uint> readInt = null;
+
             switch (offsetSize)
             {
-                case 1:
-                    curOffset = objectDataStreamOffset + (reader.ReadByte() - 1U);
-                    for (int i = 0; i < count; i++)
-                    {
-                        nextOffset = objectDataStreamOffset + (reader.ReadByte() - 1U);
-                        Objects[i] = new ObjectEntry()
-                        {
-                            Offset = curOffset,
-                            DataSize = nextOffset - curOffset,
-                        };
-
-                        curOffset = nextOffset;
-                    }
-                    break;
-
-                case 2:
-                    curOffset = objectDataStreamOffset + (reader.ReadUInt16() - 1U);
-                    for (int i = 0; i < count; i++)
-                    {
-                        nextOffset = objectDataStreamOffset + (reader.ReadUInt16() - 1U);
-                        Objects[i] = new ObjectEntry()
-                        {
-                            Offset = curOffset,
-                            DataSize = nextOffset - curOffset,
-                        };
-
-                        curOffset = nextOffset;
-                    }
-                    break;
-
-                case 4:
-                    curOffset = objectDataStreamOffset + (reader.ReadUInt32() - 1U);
-                    for (int i = 0; i < count; i++)
-                    {
-                        nextOffset = objectDataStreamOffset + (reader.ReadUInt32() - 1U);
-                        Objects[i] = new ObjectEntry()
-                        {
-                            Offset = curOffset,
-                            DataSize = nextOffset - curOffset,
-                        };
-
-                        curOffset = nextOffset;
-                    }
-                    break;
+                case 1: readInt = ReadUInt8; break;
+                case 2: readInt = ReadUInt16; break;
+                case 4: readInt = ReadUInt32; break;
             }
 
+            curOffset = objectDataStreamOffset + (readInt(reader) - 1U);
+            for (int i = 0; i < count; i++)
+            {
+                nextOffset = objectDataStreamOffset + (readInt(reader) - 1U);
+                Objects[i] = new ObjectEntry()
+                {
+                    Offset = curOffset,
+                    DataSize = nextOffset - curOffset,
+                };
+
+                curOffset = nextOffset;
+            }
             OffsetToNextBlock = nextOffset;
         }
+
+        private uint ReadUInt32(EnhancedBinaryReader reader) { return reader.ReadUInt32(); }
+
+        private uint ReadUInt16(EnhancedBinaryReader reader) { return reader.ReadUInt16(); }
+
+        private uint ReadUInt8(EnhancedBinaryReader reader) { return reader.ReadByte(); }
 
         internal class ObjectEntry
         {
