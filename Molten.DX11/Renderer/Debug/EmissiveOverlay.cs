a01e3b88c5465c78a2cf05ee1562db3dfe8c3005
Syncaidius <syncaidius@googlemail.com>
2018-04-29 13:59:53 +0100

    Added a debug overlay page for each deferred g-buffer component.

---
+++ Molten.DX11/Renderer/Debug/EmissiveOverlay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten.Graphics
+{
+    internal class EmissiveOverlay : DebugOverlayPage
+    {
+        public override void Render(SpriteFont font, RendererDX11 renderer, SpriteBatch batch, SceneRenderDataDX11 scene, IRenderSurface surface)
+        {
+            StartStep start = renderer.GetRenderStep<StartStep>();
+            batch.DrawString(font, $"Emissive", new Vector2F(3, 3), Color.Yellow);
+            batch.Draw(start.Emissive, surface.Viewport.Bounds, Color.White);
+        }
+    }
+}
---
+++ Molten.DX11/Renderer/Debug/RawSceneOverlay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten.Graphics
+{
+    internal class DrawSceneOverlay : DebugOverlayPage
+    {
+        public override void Render(SpriteFont font, RendererDX11 renderer, SpriteBatch batch, SceneRenderDataDX11 scene, IRenderSurface surface)
+        {
+            StartStep start = renderer.GetRenderStep<StartStep>();
+            batch.DrawString(font, $"Raw Scene", new Vector2F(3, 3), Color.Yellow);
+            batch.Draw(start.Scene, surface.Viewport.Bounds, Color.White);
+        }
+    }
+}
