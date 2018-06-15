b6ced4b2382cfe9c09c8ca7d16019e003cf7c503
Syncaidius <syncaidius@googlemail.com>
2018-06-15 14:19:48 +0100

    Removed UI project. Decided against separating it from the main engine.

---
+++ Molten.Engine/UI/UIComponent.cs
@@ -0,0 +1,24 @@
+﻿using Molten.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten.UI
+{
+    public abstract class UIComponent : IRenderable2D, ISceneObject
+    {
+        public Scene Scene { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Update(Timing time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Render(SpriteBatch sb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
