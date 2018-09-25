18be9061d3d43b6e9cf41473041c038e9be6d48c
Syncaidius <syncaidius@googlemail.com>
2018-09-25 22:31:40 +0100

    More work on the new sprite batcher.

---
+++ Molten.DX11/Renderer/SpriteBatcherDX11.cs
@@ -0,0 +1,77 @@
+﻿using SharpDX.Direct3D;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+
+namespace Molten.Graphics
+{
+    public class SpriteBatcherDX11 : SpriteBatcher
+    {
+        struct Range
+        {
+            public int Start;
+            public int End;
+
+            public ITexture2D Texture;
+            public IMaterial Material;
+        }
+
+        Range[] _ranges;
+        int _curRange;
+
+        internal SpriteBatcherDX11(int initialCapacity = 3000) : base(initialCapacity)
+        {
+            _ranges = new Range[100];
+        }
+
+        internal void Flush(GraphicsPipe pipe, RenderCamera camera, bool depthSort)
+        {
+            if (NextID == 0)
+                return;
+
+            Sort(camera, depthSort);
+
+            _curRange = 0;
+            _ranges[_curRange] = new Range()
+            {
+                Start = 0,
+                Texture = Sprites[0].Texture,
+                Material = Sprites[0].Material,
+            };
+
+            for (int i = 1; i < NextID; i++)
+            {
+                if(Sprites[i].Texture != _ranges[_curRange].Texture || Sprites[i].Material != _ranges[_curRange].Material)
+                {
+                    _ranges[_curRange++].End = i - 1;
+
+                    if (_curRange == _ranges.Length)
+                        Array.Resize(ref _ranges, _ranges.Length + (_ranges.Length / 2));
+
+                    _ranges[_curRange] = new Range()
+                    {
+                        Start = i,
+                        Texture = Sprites[i].Texture,
+                        Material = Sprites[i].Material,
+                    };
+                }
+
+
+                /* TODO:
+                 * 1) Accumulate ranges as above and track total vertices accumulated so far
+                 * 2) once we hit the max capacity, flush accumulated ranges
+                 * 3) use a single buffer map() call to upload accumulated ranges
+                 * 4) reset range array back to 0
+                 * 5) Accumulate the next batch of ranges
+                 * 6) Continue until we reach the end of the sprite array.
+                 * 
+                 * 
+                 */
+            }
+        }
+    }
+}
--- Molten.Render/Spritebatch/SpriteBatcher.cs
+++ Molten.Render/Spritebatch/SpriteBatcher.cs
@@ -12,28 +12,44 @@ namespace Molten.Graphics
     /// </summary>
     public abstract class SpriteBatcher
     {
-        protected class Spriteitem
+        protected class SpriteCluster
+        {
+
+        }
+
+        protected class SpriteItem : IComparable<SpriteItem>
         {
             public SpriteVertex2 Vertex;
             public ITexture2D Texture;
             public IMaterial Material;
+            public float ZKey;
+
+            public int CompareTo(SpriteItem other)
+            {
+                if (ZKey > other.ZKey)
+                    return 1;
+                else if (ZKey < other.ZKey)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+       
+        protected SpriteItem[] Sprites;
+        protected int NextID;
+
+        public SpriteBatcher(int initialCapacity)
+        {
+            Sprites = new SpriteItem[initialCapacity];
         }
 
-        Spriteitem[] _sprites;
-        int _nextID;
-
-        public SpriteBatcher(int initialCapacity = 3000)
+        protected SpriteItem GetItem()
         {
-            _sprites = new Spriteitem[initialCapacity];
-        }
+            if (NextID == Sprites.Length) // Increase length by 50%
+                Array.Resize(ref Sprites, Sprites.Length + (Sprites.Length / 2));
 
-        protected Spriteitem GetItem()
-        {
-            if (_nextID == _sprites.Length) // Increase length by 50%
-                Array.Resize(ref _sprites, _sprites.Length + (_sprites.Length / 2));
-
-            _sprites[_nextID] = _sprites[_nextID] ?? new Spriteitem();
-            return _sprites[_nextID++];
+            Sprites[NextID] = Sprites[NextID] ?? new SpriteItem();
+            return Sprites[NextID++];
         }
 
         /// <summary>
@@ -48,11 +64,13 @@ namespace Molten.Graphics
         /// <param name="origin">The origin, as a unit value. 1.0f will set the origin to the bottom-right corner of the sprite.
         /// 0.0f will set the origin to the top-left. The origin acts as the center of the sprite.</param>
         /// <param name="material"></param>
+        /// <param name="depth">The z-depth of the sprite.</param>
         public void Draw(ITexture2D texture, Rectangle source, Vector2F position, Vector2F scale, Color color, float rotation, Vector2F origin, IMaterial material, float depth = 0.0f)
         {
-            Spriteitem item = GetItem();
+            SpriteItem item = GetItem();
             item.Texture = texture;
             item.Material = material;
+            item.ZKey = depth;
 
             item.Vertex.Position = new Vector3F(position, depth);
             item.Vertex.Rotation = new Vector3F(rotation, 0, 0);
@@ -74,36 +92,35 @@ namespace Molten.Graphics
         /// <param name="origin">The origin, as a unit value. 1.0f will set the origin to the bottom-right corner of the sprite.
         /// 0.0f will set the origin to the top-left. The origin acts as the center of the sprite.</param>
         /// <param name="material"></param>
-        public void Draw(ITexture2D texture, Rectangle source, Vector3F position, Vector2F size, Color color, Vector3F rotation, Vector2F origin, IMaterial material)
+        public void Draw(ITexture2D texture, Rectangle source, Vector3F position, Vector2F scale, Color color, Vector3F rotation, Vector2F origin, IMaterial material)
         {
-            Spriteitem item = GetItem();
+            SpriteItem item = GetItem();
             item.Texture = texture;
             item.Material = material;
 
             item.Vertex.Position = position;
             item.Vertex.Rotation = rotation;
-            item.Vertex.Size = size;
+            item.Vertex.Size = new Vector2F(source.Width * scale.X, source.Height * scale.Y);
             item.Vertex.Color = color;
             item.Vertex.Origin = origin;
             item.Vertex.UV = new Vector4F(source.X, source.Y, source.Right, source.Bottom);
         }
 
-        private void Sort(RenderCamera camera)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="depthSort">If true, the Z-axis of sprites is used for sorting instead of their distance from the provided camera.</param>
+        protected void Sort(RenderCamera camera, bool depthSort = true)
         {
-            // TODO use unsafe code when sorting
-            // Store spritebatch data in class instances
-            // SEE: https://github.com/MonoGame/MonoGame/blob/6f34eb393aa0ac005888d74c5c4c6ab5615fdc8c/MonoGame.Framework/Graphics/SpriteBatcher.cs#L147
+            if (!depthSort)
+            {
+                Vector3F camPos = camera.Position;
+                for (int i = 0; i < NextID; i++)
+                    Vector3F.Distance(ref camPos, ref Sprites[i].Vertex.Position, out Sprites[i].ZKey);
+            }
 
-            Array.Sort(_sprites, 0, _nextID);
-
-            /* PROBLEMS TO SOLVE:
-             *  - List.Sort() = unstable sort (inconsistent Z-sorting), since ordering will chang eif Z-depth is equal.
-             *  - List.Orderby() = stable sort, but returns a new IEnumerable instance (garbage generation).
-             *  - for 3D sprites, there is no way to sort by depth. Need to calculate distance from camera.
-             * 
-             * 
-             * 
-             */
+            Array.Sort(Sprites, 0, NextID);
         }
     }
 }
