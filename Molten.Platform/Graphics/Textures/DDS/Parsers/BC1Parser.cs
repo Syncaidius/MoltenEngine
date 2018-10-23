993085d7b59db706d2388a517dddd9a2e6d1cf7a
James Yarwood <syncaidius@googlemail.com>
2018-10-23 21:59:45 +0100

    Cleaned up BC1 parser.

--- Molten.Platform/Graphics/Textures/DDS/Parsers/BC1Parser.cs
+++ Molten.Platform/Graphics/Textures/DDS/Parsers/BC1Parser.cs
@@ -36,30 +36,6 @@ namespace Molten.Graphics.Textures
                     }
                 }
             }
-
-            //BCColorTable table;
-            //DecodeColorTableBC1(imageReader, out table);
-
-            //// TODO use fixed pointer block here on output array.
-            //for (int bpy = 0; bpy < DDSHelper.BLOCK_DIMENSIONS; bpy++)
-            //{
-            //    int py = (dimensions.Y << 2) + bpy;
-            //    for (int bpx = 0; bpx < DDSHelper.BLOCK_DIMENSIONS; bpx++)
-            //    {
-            //        uint index = (table.data >> 2 * (4 * bpy + bpx)) & 0x03;
-            //        Color c = table.color[index];
-
-            //        int px = (dimensions.X << 2) + bpx;
-            //        if ((px < levelWidth) && (py < levelHeight))
-            //        {
-            //            int offset = ((py * levelWidth) + px) << 2;
-            //            output[offset] = c.R;
-            //            output[offset + 1] = c.G;
-            //            output[offset + 2] = c.B;
-            //            output[offset + 3] = (byte)((table.rawColor[0] <= table.rawColor[1]) && ((table.data & 0x03) == 0x03) ? 0 : 255);
-            //        }
-            //    }
-            //}
         }
 
         protected override void EncodeBlock(BinaryWriter writer, BCDimensions dimensions, TextureData.Slice level)
@@ -89,11 +65,6 @@ namespace Molten.Graphics.Textures
 
             D3DX_BC1 bc1 = BC.D3DXEncodeBC1(colTable, 1.0f, BCFlags.NONE);
             bc1.Write(writer);
-
-            //int bPixelX = dimensions.X * DDSHelper.BLOCK_DIMENSIONS;
-            //int bPixelY = dimensions.Y * DDSHelper.BLOCK_DIMENSIONS;
-            //int bytesPerPixel = 4; // Uncompressed bytes-per-pixel
-            //EncodeBC1ColorBlock(writer, level, bPixelX, bPixelY, bytesPerPixel, true, 255, dimensions);
         }
     }
 }
