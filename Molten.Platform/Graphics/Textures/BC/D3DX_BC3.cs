a97c40e349ef9af8711b9e7d0d21e0477e548a06
James Yarwood <syncaidius@googlemail.com>
2018-10-24 09:29:29 +0100

    Moved DXT5/BC3 parser over to DirectXTex-based parser.

--- Molten.Platform/Graphics/Textures/BC/D3DX_BC3.cs
+++ Molten.Platform/Graphics/Textures/BC/D3DX_BC3.cs
@@ -14,6 +14,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,9 +23,27 @@ namespace Molten.Graphics.Textures
 {
     internal class D3DX_BC3
     {
-        public byte[] alpha = new byte[2];                 // Alpha values.
+        public byte[] alpha = new byte[2];          // Alpha values.
         public byte[] bitmap = new byte[6];         // 3bpp alpha bitmap
         public D3DX_BC1 bc1 = new D3DX_BC1();       // BC1 rgb data
+
+        internal void Read(BinaryReader reader)
+        {
+            alpha[0] = reader.ReadByte();
+            alpha[1] = reader.ReadByte();
+            for (int i = 0; i < bitmap.Length; i++)
+                bitmap[i] = reader.ReadByte();
+
+            bc1.Read(reader);
+        }
+
+        internal void Write(BinaryWriter writer)
+        {
+            writer.Write(alpha[0]);
+            writer.Write(alpha[1]);
+            writer.Write(bitmap, 0, bitmap.Length);
+            bc1.Write(writer);
+        }
     };
 
 }
--- Molten.Platform/Graphics/Textures/DDS/Parsers/BC3Parser.cs
+++ Molten.Platform/Graphics/Textures/DDS/Parsers/BC3Parser.cs
@@ -10,56 +10,29 @@ namespace Molten.Graphics.Textures
     {
         public override GraphicsFormat[] SupportedFormats => new GraphicsFormat[] { GraphicsFormat.BC3_Typeless, GraphicsFormat.BC3_UNorm, GraphicsFormat.BC3_UNorm_SRgb };
 
-        protected override void DecodeBlock(BinaryReader reader, BCDimensions dimensions, int width, int height, byte[] output)
+        protected override void DecodeBlock(BinaryReader imageReader, BCDimensions dimensions, int levelWidth, int levelHeight, byte[] output)
         {
-            //===========ALPHA BLOCK==========================
-            byte alpha0 = reader.ReadByte();
-            byte alpha1 = reader.ReadByte();
-            ulong alphaMask = reader.ReadByte();
+            D3DX_BC3 bc = new D3DX_BC3();
+            bc.Read(imageReader);
+            Color4[] colTable = BC.D3DXDecodeBC3(bc);
 
-            alphaMask += (ulong)reader.ReadByte() << 8;
-            alphaMask += (ulong)reader.ReadByte() << 16;
-            alphaMask += (ulong)reader.ReadByte() << 24;
-            alphaMask += (ulong)reader.ReadByte() << 32;
-            alphaMask += (ulong)reader.ReadByte() << 40;
-
-            //============COLOR BLOCK=========================
-            BCColorTable table;
-            DecodeColorTableBC1(reader, out table);
-
-            // Decompress pixel data from block
-            for (int pby = 0; pby < DDSHelper.BLOCK_DIMENSIONS; pby++)
+            // TODO use fixed pointer block here on output array.
+            int index = 0;
+            for (int bpy = 0; bpy < DDSHelper.BLOCK_DIMENSIONS; bpy++)
             {
-                int py = (dimensions.Y << 2) + pby;
-
-                for (int pbx = 0; pbx < DDSHelper.BLOCK_DIMENSIONS; pbx++)
+                int py = (dimensions.Y << 2) + bpy;
+                for (int bpx = 0; bpx < DDSHelper.BLOCK_DIMENSIONS; bpx++)
                 {
-                    uint index = (table.data >> 2 * (4 * pby + pbx)) & 0x03;
-                    uint alphaIndex = (uint)((alphaMask >> 3 * (4 * pby + pbx)) & 0x07);
-                    Color c = table.color[index];
+                    Color c = (Color)colTable[index++];
 
-                    // Decode alpha
-                    if (alphaIndex == 0)
-                        c.A = alpha0;
-                    else if (alphaIndex == 1)
-                        c.A = alpha1;
-                    else if (alpha0 > alpha1)
-                        c.A = (byte)(((8 - alphaIndex) * alpha0 + (alphaIndex - 1) * alpha1) / 7f);
-                    else if (alphaIndex == 6)
-                        c.A = 0;
-                    else if (alphaIndex == 7)
-                        c.A = 255;
-                    else
-                        c.A = (byte)(((6 - alphaIndex) * alpha0 + (alphaIndex - 1) * alpha1) / 5f);
-
-                    int px = (dimensions.X << 2) + pbx;
-                    if ((px < width) && (py < height))
+                    int px = (dimensions.X << 2) + bpx;
+                    if ((px < levelWidth) && (py < levelHeight))
                     {
-                        int offset = ((py * width) + px) << 2;
+                        int offset = ((py * levelWidth) + px) << 2;
                         output[offset] = c.R;
                         output[offset + 1] = c.G;
                         output[offset + 2] = c.B;
-                        output[offset + 3] = DecodeSingleChannelColor(alphaMask, pbx, pby, alpha0, alpha1);
+                        output[offset + 3] = c.A;
                     }
                 }
             }
@@ -67,9 +40,31 @@ namespace Molten.Graphics.Textures
 
         protected override void EncodeBlock(BinaryWriter writer, BCDimensions dimensions, TextureData.Slice level)
         {
-            int bytesPerPixel = 4;
-            Encode8BitSingleChannelBlock(writer, level, ref dimensions, bytesPerPixel, 3);
-            EncodeBC1ColorBlock(writer, level, dimensions.PixelX, dimensions.PixelY, bytesPerPixel, false, 0, dimensions);
+            int index = 0;
+            Color4[] colTable = new Color4[BC.NUM_PIXELS_PER_BLOCK];
+
+            for (int bpy = 0; bpy < DDSHelper.BLOCK_DIMENSIONS; bpy++)
+            {
+                int py = (dimensions.Y << 2) + bpy;
+                for (int bpx = 0; bpx < DDSHelper.BLOCK_DIMENSIONS; bpx++)
+                {
+                    int px = (dimensions.X << 2) + bpx;
+                    if ((px < level.Width) && (py < level.Height))
+                    {
+                        int offset = ((py * level.Width) + px) << 2;
+                        colTable[index++] = new Color()
+                        {
+                            R = level.Data[offset],
+                            G = level.Data[offset + 1],
+                            B = level.Data[offset + 2],
+                            A = level.Data[offset + 3]
+                        };
+                    }
+                }
+            }
+
+            D3DX_BC3 bc = BC.D3DXEncodeBC3(colTable, BCFlags.NONE);
+            bc.Write(writer);
         }
     }
 }
