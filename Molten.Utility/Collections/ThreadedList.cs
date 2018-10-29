7f44a85239cfd0cdbc09b78b295ee173e195c0f5
James Yarwood <syncaidius@googlemail.com>
2018-10-29 15:06:52 +0000

    ThreadedList<T>.ForInterlockReverse() renamed to ForReverse(). Corrected names of missed ThreadedList<T>.For() overloads. Improved the summaries of all For_() methods for ThreadedList.

--- Molten.Utility/Collections/ThreadedList.cs
+++ Molten.Utility/Collections/ThreadedList.cs
@@ -580,37 +580,6 @@ namespace Molten.Collections
             return hasItem;
         }
 
-        /// <summary>Runs a for loop inside an interlock on the list. This allows iteration with only interlocking once. This can hurt performance if
-        /// the loop takes too long to execute while other threads are waiting to access the list. Return true from the callback to break out of the for loop.</summary>
-        /// <param name="start">The start index.</param>
-        /// <param name="increment">The increment.</param>
-        /// <param name="callback">The callback to run on each iteration. The callback can optionally return true to break out of the loop.</param>
-        public void For(int start, int increment, Action<int, T> callback)
-        {
-            _interlocker.Lock(() =>
-            {
-                for (int i = start; i < _count; i += increment)
-                    callback(i, _items[i]);
-            });
-        }
-
-        /// <summary>Runs a for loop inside an interlock on the list. This allows iteration with only interlocking once. This can hurt performance if
-        /// the loop takes too long to execute while other threads are waiting to access the list. Return true from the callback to break out of the for loop.</summary>
-        /// <param name="start">The start index.</param>
-        /// <param name="increment">The increment.</param>
-        /// <param name="callback">The callback to run on each iteration. The callback can optionally return true to break out of the loop.</param>
-        public void For(int start, int increment, Func<int, T, bool> callback)
-        {
-            _interlocker.Lock(() =>
-            {
-                for (int i = start; i < _count; i += increment)
-                {
-                    if (callback(i, _items[i]))
-                        break;
-                }
-            });
-        }
-
         /// <summary>Runs a foreach loop inside an interlock on the current <see cref="ThreadedList{T}"/> instance. This allows the collection to be iterated over in a thread-safe manner. 
         /// However, it can hurt performance if the loop takes too long to execute while other threads are waiting to access the list. Return true from the callback to break out of the for loop.</summary>
         /// <param name="callback">The callback to run on each iteration. The callback can optionally return true to break out of the loop.</param>
@@ -638,13 +607,47 @@ namespace Molten.Collections
             });
         }
 
-        /// <summary>Runs a for loop inside an interlock on the list. This allows iteration with only interlocking once. This can hurt performance if
-        /// the loop takes too long to execute while other threads are waiting to access the list. Return true from the callback to break out of the for loop.</summary>
+        /// <summary>Runs a for loop inside an interlock on the current <see cref="ThreadedList{T}"/> instance. instance. This allows the collection to be iterated over in a thread-safe manner. 
+        /// However, it can hurt performance if the loop takes too long to execute while other threads are waiting to access the list. <para/>
+        /// Return true from the callback to break out of the for loop.</summary>
+        /// <param name="start">The start index.</param>
+        /// <param name="increment">The increment.</param>
+        /// <param name="callback">The callback to run on each iteration. The callback can optionally return true to break out of the loop.</param>
+        public void For(int start, int increment, Action<int, T> callback)
+        {
+            _interlocker.Lock(() =>
+            {
+                for (int i = start; i < _count; i += increment)
+                    callback(i, _items[i]);
+            });
+        }
+
+        /// <summary>Runs a for loop inside an interlock on the current <see cref="ThreadedList{T}"/> instance. This allows the collection to be iterated over in a thread-safe manner. 
+        /// However, it can hurt performance if the loop takes too long to execute while other threads are waiting to access the list. <para/>
+        /// Return true from the callback to break out of the for loop.</summary>
+        /// <param name="start">The start index.</param>
+        /// <param name="increment">The increment.</param>
+        /// <param name="callback">The callback to run on each iteration. The callback can optionally return true to break out of the loop.</param>
+        public void For(int start, int increment, Func<int, T, bool> callback)
+        {
+            _interlocker.Lock(() =>
+            {
+                for (int i = start; i < _count; i += increment)
+                {
+                    if (callback(i, _items[i]))
+                        break;
+                }
+            });
+        }
+
+        /// <summary>Runs a for loop inside an interlock on the current <see cref="ThreadedList{T}"/> instance. This allows the collection to be iterated over in a thread-safe manner. 
+        /// However, it can hurt performance if the loop takes too long to execute while other threads are waiting to access the list. <para/>
+        /// Return true from the callback to break out of the for loop.</summary>
         /// <param name="start">The start index.</param>
         /// <param name="increment">The increment.</param>
         /// <param name="end">The element to iterate up to.</param>
-        /// <param name="callback">The callback to run on each iteration. The callback should return true to break out of the loop.</param>
-        public void ForInterlock(int start, int increment, int end, Func<int, T, bool> callback)
+        /// <param name="callback">The callback to run on each iteration. The callback can optionally return true to break out of the loop.</param>
+        public void For(int start, int increment, int end, Func<int, T, bool> callback)
         {
             _interlocker.Lock(() =>
             {
@@ -658,11 +661,30 @@ namespace Molten.Collections
             });
         }
 
-        /// <summary>Runs a for loop inside an interlock on the list, backwards. This allows iteration with only interlocking once. This can hurt performance if
-        /// the loop takes too long to execute while other threads are waiting to access the list. Return true from the callback to break out of the for loop.</summary>
+        /// <summary>Runs a for loop inside an interlock on the current <see cref="ThreadedList{T}"/> instance. This allows the collection to be iterated over in a thread-safe manner. 
+        /// However, it can hurt performance if the loop takes too long to execute while other threads are waiting to access the list. <para/>
+        /// Return true from the callback to break out of the for loop.</summary>
+        /// <param name="start">The start index.</param>
+        /// <param name="increment">The increment.</param>
+        /// <param name="end">The element to iterate up to.</param>
+        /// <param name="callback">The callback to run on each iteration. The callback can optionally return true to break out of the loop.</param>
+        public void For(int start, int increment, int end, Action<int, T> callback)
+        {
+            _interlocker.Lock(() =>
+            {
+                // Figure out which is the smallest condition value. 
+                int last = Math.Min(_count, end);
+                for (int i = start; i < last; i += increment)
+                    callback(i, _items[i]);
+            });
+        }
+
+        /// <summary>Runs a reversed for loop inside an interlock on the current <see cref="ThreadedList{T}"/> instance. This allows the collection to be iterated over in a thread-safe manner. 
+        /// However, it can hurt performance if the loop takes too long to execute while other threads are waiting to access the list. <para/>
+        /// Return true from the callback to break out of the for loop.</summary>
         /// <param name="decrement">The decremental value.</param>
-        /// <param name="callback">The callback to run on each iteration. The callback should return true to break out of the loop.</param>
-        public void ForInterlockReverse(int decrement, Func<int, T, bool> callback)
+        /// <param name="callback">The callback to run on each iteration. The callback can optionally return true to break out of the loop.</param>
+        public void ForReverse(int decrement, Func<int, T, bool> callback)
         {
             _interlocker.Lock(() =>
             {
@@ -675,12 +697,13 @@ namespace Molten.Collections
             });
         }
 
-        /// <summary>Runs a for loop inside an interlock on the list, backwards. This allows iteration with only interlocking once. This can hurt performance if
-        /// the loop takes too long to execute while other threads are waiting to access the list. Return true from the callback to break out of the for loop.</summary>
+        /// <summary>Runs a reversed for loop inside an interlock on the current <see cref="ThreadedList{T}"/> instance. This allows the collection to be iterated over in a thread-safe manner. 
+        /// However, it can hurt performance if the loop takes too long to execute while other threads are waiting to access the list. <para/>
+        /// Return true from the callback to break out of the for loop.</summary>
         /// <param name="start">The start index.</param>
         /// <param name="decrement">The decremental value.</param>
-        /// <param name="callback">The callback to run on each iteration. The callback should return true to break out of the loop.</param>
-        public void ForInterlockReverse(int start, int decrement, Func<int, T, bool> callback)
+        /// <param name="callback">The callback to run on each iteration. The callback can optionally return true to break out of the loop.</param>
+        public void ForReverse(int start, int decrement, Func<int, T, bool> callback)
         {
             _interlocker.Lock(() =>
             {
