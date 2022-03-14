c76057edb648b522aaa68984ed375235165559ff
James Yarwood <syncaidius@googlemail.com>
2022-03-14 20:12:25 +0000

    Added missing types.. What you doing Github!

---
+++ Molten.Engine/UI/IUIRenderData.cs
@@ -0,0 +1,14 @@
+﻿using Molten.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten.UI
+{
+    public interface IUIRenderData
+    {
+        void Render(SpriteBatcher sb, UIBaseData data);
+    }
+}
---
+++ Molten.Engine/UI/UIBaseData.cs
@@ -0,0 +1,36 @@
+﻿using Molten.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten.UI
+{
+    public class UIBaseData
+    {
+        internal UIBaseData() { }
+
+        /// <summary>
+        /// The ID of the parent <see cref="UIBaseData"/>. Populated internally before rendering
+        /// </summary>
+        public UIBaseData Parent;
+
+        /// <summary>
+        /// Global position of the UI component, where 0,0 is it's origin.
+        /// </summary>
+        public Rectangle GlobalBounds;
+
+        public Rectangle LocalBounds;
+
+        public Rectangle RenderBounds;
+
+        public Rectangle BorderBounds;
+
+        public UISpacing Margin = new UISpacing();
+
+        public UISpacing Padding = new UISpacing();
+
+        public UIAnchorFlags Anchor;
+    }
+}
