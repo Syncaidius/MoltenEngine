fa26989764a90bc2a94df14987b7fc7b4b82e327
James Yarwood <syncaidius@googlemail.com>
2022-07-15 09:53:15 +0100

    [UI] More work on UITheme JSON serializer

--- Molten.Engine/Content/Serialization/UIThemeConverter.cs
+++ Molten.Engine/Content/Serialization/UIThemeConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Molten.UI;
@@ -23,12 +24,50 @@ namespace Molten
             theme.Clear();
 
             JObject jObject = JObject.Load(reader);
-            Dictionary<string, JToken> entries = new Dictionary<string, JToken>();
-            IEnumerable<JProperty> children = jObject.Properties();
+            IEnumerable<JProperty> entries = jObject.Properties();
 
-            foreach(JProperty child in children)
+            foreach(JProperty entry in entries)
             {
-                entries.Add(child.Name, child);
+                UIStyle style = theme.AddStyle(entry.Name);
+
+                Dictionary<string, MemberInfo> infoByName = new Dictionary<string, MemberInfo>();
+                foreach (MemberInfo m in style.Properties.Keys)
+                    infoByName.Add(m.Name.ToLower(), m);
+
+                if (entry.HasValues)
+                {
+                    if (entry.Value.Type == JTokenType.Object)
+                    {
+                        JObject styleObj = entry.Value as JObject;
+                        IEnumerable<JProperty> styleProperties = styleObj.Properties();
+                        foreach(JProperty p in styleProperties)
+                        {
+                            switch (p.Type)
+                            {
+                                case JTokenType.Object:
+                                    // We have multiple style values for this property. e.g. Default, Pressed, etc
+                                    break;
+
+                                case JTokenType.Array:
+                                    // TODO The style values were provided as an array.
+                                    // We'll interpret them in the order matching the UIElementState enum values. e.g. Default, Pressed, Disabled, Hover, etc
+                                    break;
+
+                                default:
+                                    // TODO Assume we've been given a single value. Try to directly serialize it into the style member's 'Default' value.
+                                    break;
+                            }
+                            if (p.Type == JTokenType.Object)
+                            {
+                                
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Invalid value, each theme style entry should be a Json Object full of relevant key-values.
+                    }
+                }
             }
 
             return theme;
