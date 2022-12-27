da68c7153846ff47235d1a29be019faff72f01ac
James Yarwood <syncaidius@googlemail.com>
2022-12-27 13:35:11 +0000

    [Graphics] Added missing summaries to ShaderStageCapabilities
    [Vulkan] Added detection of shader precision features

--- Molten.Graphics.Vulkan/Hardware/DisplayManagerVK.cs
+++ Molten.Graphics.Vulkan/Hardware/DisplayManagerVK.cs
@@ -61,10 +61,24 @@ namespace Molten.Graphics
 
         private void PopulateCapabilityFeatures(GraphicsCapabilities cap, ref PhysicalDeviceFeatures features)
         {
-            cap.HullShader.IsSupported = features.TessellationShader;
-            cap.DomainShader.IsSupported = features.TessellationShader;
-            cap.GeometryShader.IsSupported = features.GeometryShader;
-         
+            bool int16 = features.ShaderInt16;
+            bool int64 = features.ShaderInt64;
+            bool float64 = features.ShaderFloat64;
+
+            SetShaderPrecision(cap.VertexShader, true, int16, int64, float64);
+            SetShaderPrecision(cap.GeometryShader, features.GeometryShader, int16, int64, float64);
+            SetShaderPrecision(cap.HullShader, features.TessellationShader, int16, int64, float64);
+            SetShaderPrecision(cap.DomainShader, features.TessellationShader, int16, int64, float64);
+            SetShaderPrecision(cap.PixelShader, true, int16, int64, float64);
+            SetShaderPrecision(cap.Compute, true, int16, int64, float64);            
+        }
+
+        private void SetShaderPrecision(ShaderStageCapabilities sCap, bool supported, bool int16, bool int64, bool float64)
+        {
+            sCap.IsSupported = supported;
+            sCap.Int16 = int16;
+            sCap.Int64 = int64;
+            sCap.Float64 = float64;
         }
 
         public void Dispose()
