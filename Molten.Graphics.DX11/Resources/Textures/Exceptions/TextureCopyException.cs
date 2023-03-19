c7bf053d6b7b4ddfd30530a1ec9eb87324e6dd55
James Yarwood <syncaidius@googlemail.com>
2023-03-19 15:04:42 +0000

    [DX11] Removed unused TextureDescriptionException

---
+++ Molten.Graphics.DX11/Resources/Textures/Exceptions/TextureCopyException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten.Graphics
+{
+    public class TextureCopyException : Exception
+    {
+        public TextureCopyException(TextureDX11 source, TextureDX11 destination)
+        : this(source, destination, "Invalid copy operation.")
+        { }
+
+        public TextureCopyException(TextureDX11 source, TextureDX11 destination, string message)
+            : base(message)
+        {
+            SourceTexture = source;
+            DestTexture = destination;
+
+        }
+
+        /// <summary>
+        /// The source texture.
+        /// </summary>
+        public TextureDX11 SourceTexture { get; private set; }
+
+        /// <summary>
+        /// The destination texture.
+        /// </summary>
+        public TextureDX11 DestTexture { get; private set; }
+    }
+}
