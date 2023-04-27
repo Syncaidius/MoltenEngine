8035683126133dccfee9c5a32fbfc4571effa0d2
James Yarwood <syncaidius@googlemail.com>
2023-04-27 08:18:13 +0100

    [Vulkan] Corrected name of SpirvResultID

---
+++ Molten.Graphics.Vulkan/Shaders/Spirv/Literals/SpirvResultID.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten.Graphics.Vulkan
+{
+    public abstract class SpirvResultID : SpirvLiteral<uint>
+    {
+        public override string ToString()
+        {
+            return $"Result ({Value})";
+        }
+    }
+}
