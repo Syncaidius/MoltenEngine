78aa528f489b3b03789c55d965521b4cc4af495b
James Yarwood <syncaidius@googlemail.com>
2023-05-22 06:49:53 +0100

    [Vulkan] Further work on shader reflection
    [Vulkan] Updated to SpirvReflector 0.1.1

--- Molten.Graphics.Vulkan/Shaders/SpirvCompiler.cs
+++ Molten.Graphics.Vulkan/Shaders/SpirvCompiler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Molten.Graphics.Dxc;
+using Silk.NET.Core.Native;
 using Silk.NET.Direct3D.Compilers;
 using Silk.NET.Vulkan;
 using SpirvReflector;
@@ -57,7 +58,12 @@ namespace Molten.Graphics.Vulkan
         protected override unsafe ShaderReflection OnBuildReflection(ShaderCompilerContext context, IDxcBlob* byteCode, DxcBuffer* reflectionBuffer)
         {
             SpirvReflection reflection = new SpirvReflection(_logger);
-            SpirvReflectionResult result = reflection.Reflect(byteCode->GetBufferPointer(), byteCode->GetBufferSize());
+            SpirvReflectionResult rr = reflection.Reflect(byteCode->GetBufferPointer(), byteCode->GetBufferSize());
+
+            ShaderReflection result = new ShaderReflection()
+            {
+                GSInputPrimitive = GeometryHullTopology.Triangle, // TODO populate
+            };
 
             //using (FileStream stream = new FileStream(context.Source.Filename + ".spirv", FileMode.Create, FileAccess.Write))
             //{
@@ -68,7 +74,148 @@ namespace Molten.Graphics.Vulkan
             //    }
             //}
 
-            throw new NotImplementedException();
+            // TODO get input/output resource bindings
+
+            // Populate input/output resource parameters
+            foreach (SpirvEntryPoint ep in rr.EntryPoints)
+            {
+                PopulateReflectionParamters(result, ep, ShaderIOStructureType.Input);
+                PopulateReflectionParamters(result, ep, ShaderIOStructureType.Output);
+
+                break; //  TODO support multiple entry points in reflection
+            }
+
+            return result;
+        }
+
+        private void PopulateReflectionParamters(ShaderReflection result, SpirvEntryPoint ep, ShaderIOStructureType type)
+        {
+            List<ShaderParameterInfo> parameters;
+            IReadOnlyList<SpirvVariable> variables;
+
+            switch (type)
+            {
+                case ShaderIOStructureType.Input:
+                    variables = ep.Inputs;
+                    parameters = result.InputParameters;
+                    break;
+
+                case ShaderIOStructureType.Output:
+                    variables = ep.Outputs;
+                    parameters = result.OutputParameters;
+                    break;
+
+                default:
+                    return;
+            }
+
+            for (int i = 0; i < variables.Count; i++)
+            {
+                SpirvVariable v = variables[i];
+
+                ShaderParameterInfo p = new ShaderParameterInfo()
+                {
+                    ComponentType = ShaderRegisterType.UInt32,
+                    Mask = ShaderComponentMaskFlags.All,
+                    ReadWriteMask = 255,
+                    MinPrecision = GetMinPrecision(v.Type),
+                    Register = v.Binding,
+                    SemanticIndex = v.Binding,
+                    Stream = 0,
+                };
+
+                ProcessDecorations(p, v);
+                p.SystemValueType = GetSystemValue(p.SemanticName);
+                parameters.Add(p);
+            }
+        }
+
+        private ShaderMinPrecision GetMinPrecision(SpirvType type)
+        {
+            switch (type.Kind)
+            {
+                case SpirvTypeKind.Int:
+                    switch (type.NumBytes)
+                    {
+                        default: return ShaderMinPrecision.Default;
+                        case 2: return ShaderMinPrecision.Sint16;
+                        case 10: return ShaderMinPrecision.Any10;
+                    }
+
+                case SpirvTypeKind.Float:
+                    switch (type.NumBytes)
+                    {
+                        default: throw new Exception($"Unsupported float length: {type.NumBytes}");
+                        case 2: return ShaderMinPrecision.Float16;
+                        case 10: return ShaderMinPrecision.Float28;
+                    }
+
+                case SpirvTypeKind.UInt:
+                    switch (type.NumBytes)
+                    {
+                        default: return ShaderMinPrecision.Default;
+                        case 2: return ShaderMinPrecision.Uint16;
+                        case 10: return ShaderMinPrecision.Any10;
+                    }
+
+                default:
+                    return ShaderMinPrecision.Default;
+            }  
+        }
+
+        private unsafe void ProcessDecorations(ShaderParameterInfo p, SpirvVariable v)
+        {
+            // Prioritize user semantics
+            foreach (SpirvDecoration dec in v.Decorations.Keys)
+            {
+                IReadOnlyList<object> parameters = v.Decorations[dec];
+
+                switch (dec)
+                {
+                    case SpirvDecoration.UserSemantic:
+                        p.SemanticName = parameters[0] as string;
+                        break;
+                }
+            }
+
+            // Now for built-in semantics.
+            if (string.IsNullOrWhiteSpace(p.SemanticName))
+            {
+                foreach (SpirvDecoration dec in v.Decorations.Keys)
+                {
+                    IReadOnlyList<object> parameters = v.Decorations[dec];
+
+                    switch (dec)
+                    {
+                        case SpirvDecoration.BuiltIn:
+                            SpirvBuiltIn bi = (SpirvBuiltIn)parameters[0];
+                            p.SemanticName = BuiltInToSemantic(bi);
+                            break;
+                    }
+                }
+            }
+
+            p.SemanticNamePtr = (void*)SilkMarshal.StringToPtr(p.SemanticName, NativeStringEncoding.UTF8);
+        }
+
+        private string BuiltInToSemantic(SpirvBuiltIn builtIn)
+        {
+            switch (builtIn)
+            {
+                default:
+                    return $"GL_{builtIn.ToString().ToUpper()}";
+
+                case SpirvBuiltIn.VertexId:
+                    return "SV_VERTEXID";
+            }
+        }
+
+        private ShaderSVType GetSystemValue(string semanticName)
+        {
+            if (!semanticName.StartsWith("SV_") || !Enum.TryParse(semanticName.Substring(3), out ShaderSVType result))
+                return ShaderSVType.Undefined;
+
+            return result;
         }
     }
 }
