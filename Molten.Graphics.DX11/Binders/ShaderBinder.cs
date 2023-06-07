fb72f10402ccb2d59484efb6c11d30002ccc25e5
James Yarwood <syncaidius@googlemail.com>
2023-06-07 21:35:06 +0100

    [DX11] Removed obsolete ShaderBinder

--- Molten.Graphics.DX11/Binders/ShaderBinder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Molten.Graphics.DX11
-{
-    internal unsafe class ShaderBinder : GraphicsSlotBinder<HlslShader>
-    {
-        public override void Bind(GraphicsSlot<HlslShader> slot, HlslShader value) { }
-
-        public override void Unbind(GraphicsSlot<HlslShader> slot, HlslShader value) { }
-    }
-}
