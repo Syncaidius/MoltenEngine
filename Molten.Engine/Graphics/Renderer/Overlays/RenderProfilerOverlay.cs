0e39f21c34516eb149a1fd44509e455abf0edae1
James Yarwood <syncaidius@googlemail.com>
2023-06-10 14:12:32 +0100

    [Graphics] Further improvements to RenderProfilerOverlay

--- Molten.Engine/Graphics/Renderer/Overlays/RenderProfilerOverlay.cs
+++ Molten.Engine/Graphics/Renderer/Overlays/RenderProfilerOverlay.cs
@@ -4,6 +4,12 @@
     {
         ulong _lastFrameID = ulong.MaxValue;
         Color _colText;
+        RectStyle _bgStyle = new RectStyle()
+        {
+            FillColor = new Color(30, 30, 30, 220),
+            BorderColor = new Color(120, 120, 120, 255),
+            BorderThickness = new Thickness(1),
+        };
 
         internal RenderProfilerOverlay()
         {
@@ -12,23 +18,31 @@
 
         public void OnRender(Timing time, SpriteBatcher sb, SpriteFont font, RenderProfiler rendererProfiler, RenderProfiler sceneProfiler, RenderCamera camera)
         {
-            Vector2F textPos = new Vector2F(5, 5);
+            Vector2F textPos = new Vector2F(20, 20);
             float lineHeight = font.GetHeight('|');
-            float graphWidth = 400;
 
             // Renderer frame profiler stats.
             RenderProfiler.Snapshot frame = rendererProfiler.Previous;
             if (frame != null)
             {
-                sb.DrawString(font, "[LAST FRAME]", textPos, _colText);
-                textPos.X += 5;
-                DrawStats(time, sb, font, frame, ref textPos, lineHeight);
-                textPos.X -= 5;
+                DrawStats(time, sb, font, frame, ref textPos, lineHeight, "[FRAME]");
+                textPos.Y += 45;
             }
+
+            // Scene profiler stats.
+            frame = sceneProfiler.Previous;
+            if (frame != null)
+                DrawStats(time, sb, font, frame, ref textPos, lineHeight, "[SCENE]");
         }
 
-        private void DrawStats(Timing time, SpriteBatcher sb, SpriteFont font, RenderProfiler.Snapshot frame, ref Vector2F textPos, float lineHeight)
+        private void DrawStats(Timing time, SpriteBatcher sb, SpriteFont font, RenderProfiler.Snapshot frame, ref Vector2F textPos, float lineHeight, string title)
         {
+            RectangleF r = new RectangleF(textPos, new Vector2F(200, lineHeight * 18));
+            r.Inflate(8);
+            sb.DrawRect(r, ref _bgStyle);
+
+            sb.DrawString(font, title, textPos, _colText);
+            textPos.Y += 5;
             textPos.Y += lineHeight; sb.DrawString(font, $"FPS: {time.FPS} -- Frame: {time.FrameID}", textPos, _colText);
             textPos.Y += lineHeight; sb.DrawString(font, $"Time: {frame.Time.ToString("N2")}ms", textPos, _colText);
             textPos.Y += lineHeight; sb.DrawString(font, $"Target: {frame.TargetTime.ToString("N2")}ms", textPos, _colText);
