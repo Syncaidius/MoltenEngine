2af3ecc9d8a229d77af05deae23409d8b8214b17
James Yarwood <syncaidius@googlemail.com>
2023-07-22 22:15:28 +0100

    [Vulkan] Implemented SamplerVK class

--- Molten.Graphics.Vulkan/Pipeline/SamplerVK.cs
+++ Molten.Graphics.Vulkan/Pipeline/SamplerVK.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Silk.NET.Vulkan;
 
@@ -10,16 +11,117 @@ namespace Molten.Graphics.Vulkan
     {
         Sampler _handle;
 
-        public SamplerVK(GraphicsDevice device, ref ShaderSamplerParameters parameters) : 
+        public unsafe SamplerVK(GraphicsDevice device, ref ShaderSamplerParameters parameters) : 
             base(device, ref parameters)
         {
+            SamplerCustomBorderColorCreateInfoEXT* customColor = null;
+
+            // If any of the address modes use border, we need to provide the custom border color extension info.
+            if (parameters.AddressU == SamplerAddressMode.Border
+                || parameters.AddressV == SamplerAddressMode.Border
+                || parameters.AddressW == SamplerAddressMode.Border)
+            {
+                customColor = EngineUtil.Alloc<SamplerCustomBorderColorCreateInfoEXT>();
+                customColor[0] = new SamplerCustomBorderColorCreateInfoEXT()
+                {
+                    SType = StructureType.SamplerCustomBorderColorCreateInfoExt,
+                    CustomBorderColor = Unsafe.As<Color4, ClearColorValue>(ref parameters.BorderColor),
+                    Format = Format.R32G32B32A32Sfloat,
+                    PNext = null
+                };
+            }
+
             SamplerCreateInfo info = new SamplerCreateInfo()
             {
                 SType = StructureType.SamplerCreateInfo,
-                
+                CompareEnable = parameters.IsComparison,
+                AnisotropyEnable = parameters.MaxAnisotropy > 0,
+                MaxAnisotropy = Math.Max(1, parameters.MaxAnisotropy),
+                MinLod = parameters.MinMipMapLod,
+                MaxLod = parameters.MaxMipMapLod,
+                MipLodBias = parameters.LodBias,
+                AddressModeU = GetAddressMode(parameters.AddressU),
+                AddressModeV = GetAddressMode(parameters.AddressV),
+                AddressModeW = GetAddressMode(parameters.AddressW),
+                MinFilter = GetFilter(parameters.MinFilter),
+                MagFilter = GetFilter(parameters.MagFilter),
+                BorderColor = BorderColor.FloatCustomExt,
+                Flags = SamplerCreateFlags.None,
+                CompareOp = GetCompareOp(parameters.Comparison),
+                MipmapMode = GetMipMode(parameters.MipFilter),
+                PNext = customColor,
+                UnnormalizedCoordinates = false, // TODO Is this point sampling?
             };
+        }
 
-           // DirectX Point-sampling is equivilent to enabling un
+        private CompareOp GetCompareOp(ComparisonMode mode)
+        {
+            switch (mode)
+            {
+                case ComparisonMode.Less: return CompareOp.Less;
+                case ComparisonMode.LessEqual: return CompareOp.LessOrEqual;
+                case ComparisonMode.Greater: return CompareOp.Greater;
+                case ComparisonMode.GreaterEqual: return CompareOp.GreaterOrEqual;
+                case ComparisonMode.Equal: return CompareOp.Equal;
+                case ComparisonMode.NotEqual: return CompareOp.NotEqual;
+                case ComparisonMode.Always: return CompareOp.Always;
+                case ComparisonMode.Never: return CompareOp.Never;
+                default: throw new Exception("Unsupported comparison mode: " + mode);
+            }
+        }
+
+        private Silk.NET.Vulkan.SamplerAddressMode GetAddressMode(SamplerAddressMode mode)
+        {
+            switch (mode)
+            {
+                case SamplerAddressMode.Wrap:
+                    return Silk.NET.Vulkan.SamplerAddressMode.Repeat;
+
+                case SamplerAddressMode.Mirror:
+                    return Silk.NET.Vulkan.SamplerAddressMode.MirroredRepeat;
+
+                case SamplerAddressMode.Clamp:
+                    return Silk.NET.Vulkan.SamplerAddressMode.ClampToEdge;
+
+                case SamplerAddressMode.MirrorOnce:
+                    return Silk.NET.Vulkan.SamplerAddressMode.MirrorClampToEdge;
+
+                case SamplerAddressMode.Border:
+                    return Silk.NET.Vulkan.SamplerAddressMode.ClampToBorder;
+
+                default:
+                    throw new Exception("Unsupported sampler address mode: " + mode);
+            }
+        }
+
+        private Filter GetFilter(SamplerFilter filter)
+        {
+            switch (filter)
+            {
+                case SamplerFilter.Linear:
+                    return Filter.Linear;
+
+                case SamplerFilter.Point:
+                    return Filter.Nearest;
+
+                default:
+                    throw new Exception($"Unsupported sampler filter: {filter}");
+            }
+        }
+
+        private SamplerMipmapMode GetMipMode(SamplerFilter filter)
+        {
+            switch (filter)
+            {
+                case SamplerFilter.Linear:
+                    return SamplerMipmapMode.Linear;
+
+                case SamplerFilter.Point:
+                    return SamplerMipmapMode.Nearest;
+
+                default:
+                    throw new Exception($"Unsupported mip-map mode: {filter}");
+            }
         }
 
         protected unsafe override void OnGraphicsRelease()
