06f9174a6d721b7a5d1c622f558f7f18e395ca2a
James Yarwood <syncaidius@googlemail.com>
2024-02-27 08:42:51 +0000

    [DX12] ResourceHandleDX12 now supports multiple resource pointers

--- Molten.Graphics.DX12/Resources/ResourceHandleDX12.cs
+++ Molten.Graphics.DX12/Resources/ResourceHandleDX12.cs
@@ -4,9 +4,9 @@ namespace Molten.Graphics.DX12;
 
 public unsafe class ResourceHandleDX12 : GraphicsResourceHandle
 {
-    ID3D12Resource1* _ptr;
+    ID3D12Resource1*[] _ptr;
 
-    internal ResourceHandleDX12(GraphicsResource resource, ID3D12Resource1* ptr) : base(resource)
+    internal ResourceHandleDX12(GraphicsResource resource, params ID3D12Resource1*[] ptr) : base(resource)
     {
         _ptr = ptr;
         Device = resource.Device as DeviceDX12;
@@ -18,31 +18,28 @@ public unsafe class ResourceHandleDX12 : GraphicsResourceHandle
             UAV = new UAViewDX12(this);
     }
 
-    internal void UpdateResource(ID3D12Resource1* ptr)
+    internal void UpdateResource(ID3D12Resource1* ptr, uint ptrIndex = 0)
     {
-        _ptr = ptr;
+        _ptr[ptrIndex] = ptr;
     }
 
     public override void Dispose()
     {
         SRV?.Dispose();
         UAV?.Dispose();
-        NativeUtil.ReleasePtr(ref _ptr);
+
+        for (int i = 0; i < _ptr.Length; i++)
+            NativeUtil.ReleasePtr(ref _ptr[i]);
     }
 
     public static implicit operator ID3D12Resource1*(ResourceHandleDX12 handle)
     {
-        return handle._ptr;
+        return handle._ptr[handle.PtrIndex];
     }
 
     public static implicit operator ID3D12Resource*(ResourceHandleDX12 handle)
     {
-        return (ID3D12Resource*)handle._ptr;
-    }
-
-    protected void SetHandle(ID3D12Resource1* ptr)
-    {
-        _ptr = ptr;
+        return (ID3D12Resource*)handle._ptr[handle.PtrIndex];
     }
 
     internal SRViewDX12 SRV { get; }
@@ -51,32 +48,11 @@ public unsafe class ResourceHandleDX12 : GraphicsResourceHandle
 
     internal DeviceDX12 Device { get; }
 
-    public unsafe ID3D12Resource1* Ptr1 => _ptr;
+    public unsafe ID3D12Resource1* Ptr1 => _ptr[PtrIndex];
 
-    public unsafe ID3D12Resource* Ptr => (ID3D12Resource*)_ptr;
-}
+    public unsafe ID3D12Resource* Ptr => (ID3D12Resource*)_ptr[PtrIndex];
 
-public class ResourceHandleDX12<V, VD> : ResourceHandleDX12
-    where V : ViewDX12<VD>, new()
-    where VD : unmanaged
-{
-    internal unsafe ResourceHandleDX12(GraphicsResource resource, ID3D12Resource1* ptr) :
-        base(resource, ptr)
-    {
-        VD desc = new VD();
-        View = new V();
-        View.Initialize(this, ref desc);
-    }
+    public uint PtrIndex { get; set; }
 
-    internal unsafe ResourceHandleDX12(GraphicsResource resource, ID3D12Resource1* ptr, ref VD desc) :
-        base(resource, ptr)
-    {
-        View = new V();
-        View.Initialize(this, ref desc);
-    }
-
-    /// <summary>
-    /// An additional, unique view of the resource, with a specific description.
-    /// </summary>
-    internal ViewDX12<VD> View { get; }
+    public int NumResources => _ptr.Length;
 }
